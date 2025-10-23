ea3659806e802eb8f685bf841b695a9f15390e0b
TasosPat <tasos.pat@outlook.com>
2025-10-23 22:07:33 +0100

    plugged in new routes for verify, logout and refresh

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, logout, verify, refresh } from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/verify", verify);
+router.get("/refresh", refresh);
+router.post("/logout", logout);
 
 export default router;
