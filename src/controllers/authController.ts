3804da90d23986b0e0dde342ae7fbf6b595a8ab7
TasosPat <tasos.pat@outlook.com>
2025-10-23 22:10:29 +0100

    added email to access token payload

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -37,6 +37,7 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     res.status(500).json({ message: "Server error" });
   }
 };
+
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
@@ -52,7 +53,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     if (!match) return res.status(400).json({ message: "Invalid credentials" });
 
     // Access token (short-lived)
-    const accessToken = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, { expiresIn: "15m" });
+    const accessToken = jwt.sign({ id: user.id, email: user.email, role: user.role }, JWT_SECRET, { expiresIn: "15m" });
 
     // Refresh token (long-lived)
     const refreshToken = jwt.sign({ id: user.id }, JWT_REFRESH_SECRET, { expiresIn: "30d" });
@@ -83,9 +84,80 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     res.status(500).json({ message: "Server error" });
   }
 };
+
 export const logout = async (req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        const refreshTokenFromCookie = req.cookies.refresh_token
+        if(refreshTokenFromCookie) {
+        await db.query("DELETE FROM tokens WHERE token = $1", [refreshTokenFromCookie]);
+        }
+        res
+        .clearCookie("access_token")
+        .clearCookie("refresh_token")
+        .json({ message: "Logged out" })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      }
 };
-export const verify = async (req: Request, res: Response, next: NextFunction) => {
 
+export const verify = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.access_token
+        if (!token) {
+            return res.status(401).json({ valid: false, message: "No refresh token found" });
+          }
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET)
+
+        res.status(200).json({
+            valid: true,
+            user: { id: (decoded as any).id, role: (decoded as any).role }
+          });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      }
+};
+
+export const refresh = async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refresh_token
+    if (!refreshToken) {
+        return res
+        .clearCookie("access_token")
+        .clearCookie("refresh_token")
+        .status(401).json({ valid: false, message: "No token found" });
+      }
+try {
+    const decoded = jwt.verify(refreshToken, JWT_REFRESH_SECRET)
+    const id = (decoded as any).id
+    
+    const result = await db.query("SELECT * FROM tokens WHERE token = $1", [refreshToken])
+    if (result.rows.length === 0) {
+        return res
+        .clearCookie("access_token")
+        .clearCookie("refresh_token")
+        .status(401).json({ valid: false, message: "No refresh token found" });
+    }
+    const userQuery = await db.query("SELECT * FROM users WHERE id = $1", [id])
+    const user = userQuery.rows[0]
+    if (!user) {
+        res.clearCookie("access_token").clearCookie("refresh_token");
+        return res.status(401).json({ valid: false, message: "User not found" });
+      }
+    const accessToken = jwt.sign({ id: user.id, email: user.email, role: user.role }, JWT_SECRET, { expiresIn: "15m" });
+    res
+    .cookie("access_token", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 15 * 60 * 1000,
+        sameSite: "lax",
+      })
+      .json({ message: "Session Refreshed", user: { id: user.id, email: user.email, role: user.role } });
+} catch (err: any) {
+    if (err.name === "TokenExpiredError" || err.name === "JsonWebTokenError") {
+        return res.status(401).json({ valid: false, message: "Invalid or expired token" });
+      }
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+  }
 };
