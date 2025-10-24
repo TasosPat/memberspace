436b8b0fe45955ea621a959fa279b587fc5d5179
TasosPat <tasos.pat@outlook.com>
2025-10-24 19:52:02 +0100

    created new routes for email verification, request password reset and password reset

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, logout, verify, refresh } from "../controllers/authController";
+import { register, login, logout, verify, refresh, verifyEmail, requestPasswordReset, resetPassword } from "../controllers/authController";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post("/login", login);
 router.get("/verify", verify);
 router.get("/refresh", refresh);
 router.post("/logout", logout);
+router.get("/verify-email", verifyEmail);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
 
 export default router;
