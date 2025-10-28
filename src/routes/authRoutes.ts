e6df930d1898e57567752aec42f74eea8ee157bf
TasosPat <tasos.pat@outlook.com>
2025-10-28 19:09:39 +0000

    added authorise middleware to verify and logout route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import { register, login, logout, verify, refresh, verifyEmail, requestPasswordReset, resetPassword } from "../controllers/authController";
+import { authenticate } from "../middleware/authorise";
+
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/verify", verify);
-router.get("/refresh", refresh);
-router.post("/logout", logout);
 router.get("/verify-email", verifyEmail);
 router.post("/request-password-reset", requestPasswordReset);
 router.post("/reset-password", resetPassword);
+router.get("/refresh", refresh);
+
+router.get("/verify", authenticate, verify);
+router.post("/logout", authenticate, logout);
 
 export default router;
